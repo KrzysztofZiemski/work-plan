a25f51ef55429c8ff5d547f24117e32982a0fcb7
Krzysztof Ziemski <krzysztofziemski@wp.pl>
2020-05-14 22:31:58 +0200

    rebuild top navbar, start create left navbar

--- src/App.js
+++ src/App.js
@@ -4,41 +4,66 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
 import { DndProvider } from 'react-dnd';
 import Backend from 'react-dnd-html5-backend';
 
 import GraphicPage from './templates/GraphicPage/GraphicPage';
 import HomePage from './templates/HomePage/HomePage';
-import MainNav from './components/MainNav';
-
+import NavBarTop from './components/NavBarTop';
+import NavBarLeft from './components/NavBarLeft';
 import './App.scss';
 
+const useStyles = makeStyles((theme) => ({
+  container: {
+    flexGrow: 1
+  },
+  hiddenMenu: {
+    width: 0,
+    transition: '.5s'
+  },
+  activeMenu: {
+    width: '200px',
+    transition: '.5s'
+  }
+}))
 function App() {
 
-  let [leftMenu, setLeftMenu] = useState(false);
+  let [activeLeftMenu, setActiveLeftMenu] = useState(true);
+  const classes = useStyles();
 
   const toggleLeftMenu = () => {
-    if (leftMenu) return setLeftMenu(false);
-    setLeftMenu(true);
+    if (activeLeftMenu) return setActiveLeftMenu(false);
+    setActiveLeftMenu(true);
   }
 
   return (
 
     <Router>
       <div className="App" >
-        <MainNav className='mainNavbar' onClick={toggleLeftMenu} />
-        <Switch>
-          <DndProvider backend={Backend}>
+        <Grid container>
+          <Grid item>
+            <NavBarLeft isActive={activeLeftMenu} />
+          </Grid>
+          <Grid item className={classes.container}>
+            <NavBarTop className='mainNavbar' onClick={toggleLeftMenu} />
 
-            <Route path="/" exact={true}>
-              <HomePage className='page' />
-            </Route>
-            <Route path="/graphic">
-              <GraphicPage className='page' />
-            </Route>
+            <Switch>
+              <DndProvider backend={Backend}>
+                <Route path="/" exact={true}>
+                  <HomePage className='page' />
+                </Route>
+                <Route path="/graphic">
+                  <GraphicPage className='page' />
+                </Route>
 
-          </DndProvider>
-        </Switch>
+              </DndProvider>
+            </Switch>
+          </Grid>
+        </Grid>
       </div>
     </Router >
 
---
+++ src/components/NavBarTop/NavBarTop.js
@@ -0,0 +1,128 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
+
+//test loggedUser
+const loggedUser = true;
+const useStyles = makeStyles((theme) => ({
+    root: {
+        background: '#3c8dbc'
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },
+    },
+
+    sectionDesktop: {
+        display: 'none',
+        [theme.breakpoints.up('md')]: {
+            display: 'flex',
+        },
+    },
+    button: {
+        background: '#fff',
+    }
+
+}));
+
+export const NavBarTop = ({ onClick }) => {
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const isMenuOpen = Boolean(anchorEl);
+
+    const handleProfileMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
+
+
+    const menuId = 'primary-search-account-menu';
+    const renderMenu = (
+        <Menu
+            anchorEl={anchorEl}
+            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+            id={menuId}
+            keepMounted
+            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+            open={isMenuOpen}
+            onClose={handleMenuClose}
+        >
+            <MenuItem component={Link} to='/settings' onClick={handleMenuClose}>Ustawienia</MenuItem>
+            <MenuItem component={Link} to='logout' onClick={handleMenuClose}>Wyloguj</MenuItem>
+        </Menu>
+    );
+
+
+
+    return (
+        <div className={classes.grow}>
+            <AppBar position="static" className={classes.root}>
+                <Toolbar>
+                    <IconButton
+                        onClick={onClick}
+                        edge="start"
+                        className={classes.menuButton}
+                        color="inherit"
+                        aria-label="open drawer"
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography className={classes.title} variant="h6" noWrap>
+                        Nazwa firmy
+                    </Typography>
+
+                    <div className={classes.grow} />
+                    <div className={classes.sectionDesktop}>
+                        {/* <IconButton aria-label="show 4 new mails" color="inherit">
+                            <Badge badgeContent={4} color="secondary">
+                                <MailIcon />
+                            </Badge>
+                        </IconButton> */}
+                        {/* <IconButton aria-label="show 17 new notifications" color="inherit">
+                            <Badge badgeContent={17} color="secondary">
+                                <NotificationsIcon />
+                            </Badge>
+                        </IconButton> */}
+                        {loggedUser ?
+                            <IconButton
+                                edge="end"
+                                aria-label="account of current user"
+                                aria-controls={menuId}
+                                aria-haspopup="true"
+                                onClick={handleProfileMenuOpen}
+                                color="inherit"
+                            >
+                                <AccountCircle />
+                            </IconButton> : <Button component={Link} to='/login' className={classes.button} variant="contained" >
+                                Zaloguj
+                            </Button>
+
+                        }
+                    </div>
+                </Toolbar>
+            </AppBar>
+            {renderMenu}
+        </div>
+    );
+}
